e15c5885db4097dfda1c214b249d0cc62fc4ed34
Abhay Singh <as9588211@gmail.com>
2026-06-05 00:08:47 +0530

    Day 1: Data ingestion complete

---
+++ data_ingestion.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+files = [
+    "01_fund_master.csv",
+    "02_nav_history.csv",
+    "03_aum_by_fund_house.csv",
+    "04_monthly_sip_inflows.csv",
+    "05_category_inflows.csv",
+    "06_industry_folio_count.csv",
+    "07_scheme_performance.csv",
+    "08_investor_transactions.csv",
+    "09_portfolio_holdings.csv",
+    "10_benchmark_indices.csv"
+]
+
+for file in files:
+
+    print("\n" + "="*60)
+    print("DATASET :", file)
+
+    df = pd.read_csv(f"data/raw/{file}")
+
+    print("\nShape:")
+    print(df.shape)
+
+    print("\nData Types:")
+    print(df.dtypes)
+
+    print("\nFirst 5 Rows:")
+    print(df.head())
---
+++ live_nav_fetch.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+url = "https://api.mfapi.in/mf/125497"
+
+response = requests.get(url)
+
+data = response.json()
+
+print("Scheme Name:")
+print(data["meta"]["scheme_name"])
+
+nav_df = pd.DataFrame(data["data"])
+
+print(nav_df.head())
+
+nav_df.to_csv(
+    "data/raw/hdfc_top100_live_nav.csv",
+    index=False
+)
+
+print("CSV Saved Successfully")
